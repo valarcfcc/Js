f1bee8bad3b9fcf7107197383a8bdce63436a241
valarcfcc <chengfan001@hotmail.com>
2020-07-30 20:04:32 +0800

    array

--- src/js/array.js
+++ src/js/array.js
@@ -9,3 +9,16 @@ console.log(colors3);
 
 var colors4 = colors2.slice(1, 4);
 console.log(colors4);
+// 删除
+var removed = colors2.splice(0, 1);
+console.log(colors2);
+console.log(removed);
+
+var removed = colors2.splice(1, 0, "red", "orange");
+console.log(colors2);
+console.log(removed);
+
+
+var removed = colors2.splice(1, 1, "red", "orange");
+console.log(colors2);
+console.log(removed);
