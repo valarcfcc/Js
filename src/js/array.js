ad69ef0de7cd2d0619cadaca916b4b221a915b8a
valarcfcc <chengfan001@hotmail.com>
2020-07-30 20:13:03 +0800

    array

--- src/js/array.js
+++ src/js/array.js
@@ -22,3 +22,9 @@ console.log(removed);
 var removed = colors2.splice(1, 1, "red", "orange");
 console.log(colors2);
 console.log(removed);
+
+var numbers = [1, 23, 4, 5, 6, 7, 8, 9, 0, 5, 4, 343, 34, 23, ];
+console.log(numbers.indexOf(4));
+console.log(numbers.indexOf(4, 4));
+console.log(numbers.lastIndexOf(4));
+console.log(numbers.lastIndexOf(4, 4));
